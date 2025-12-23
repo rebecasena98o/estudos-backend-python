d1a01646d12d4b6d8fc1dd6c643444d0a4c82af3
Rebeca Sena <r3b3casena@gmail.com>
2025-12-23 01:14:38 -0300

    fix: adicionando arquivo operadores_Aritmeticos.py

---
+++ operadores_Aritmeticos.py
@@ -0,0 +1,42 @@
+produto1 = 20
+produto2 = 10
+
+print(produto1 + produto2) #adição
+
+print("Soma: ", produto1 + produto2) #sugestão do vscode
+
+print(produto1 - produto2) #subtração
+
+print("Subtração: ", produto1 - produto2) #subtração
+
+print(produto1 / produto2) #divisão
+
+print("Divisão: ", produto2 / produto1) #divisão
+
+print(produto1 // produto2) #divisão inteira
+
+print("Divisão Inteira: ", produto2 // produto1) #divisão inteira
+
+print(produto1 * produto2) #multiplicação
+
+print("Multiplicação: ", produto1 * produto2) #multiplicação
+
+print(produto2 % produto1) #módulo (resto da divisão)
+
+print("Módulo: ", produto2 % produto1) #módulo (resto da divisão)
+
+print(produto1 ** 2) #exponenciação
+
+print("Exponenciação: ", produto1 ** 2) #exponenciação
+
+x = 10 + 5 + 4
+print(x)
+
+y = (10 + 5) * 4
+print(y)
+
+z = 10 + (5 * 4)
+print(z)
+
+A = (10 / 2) + (25 * 2) - (2 ** 2) #esquerda p/ direita
+print(A)
