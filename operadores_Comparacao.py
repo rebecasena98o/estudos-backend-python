49ed26196246059a72958c28c1716a4f5c04019f
Rebeca Sena <r3b3casena@gmail.com>
2025-12-23 11:58:32 -0300

    feat: adicionando arquivo de exemplos de comparações

---
+++ operadores_Comparacao.py
@@ -0,0 +1,31 @@
+saldo = 200
+saque = 200
+
+print(saldo == saque)
+
+print(saldo != saque)
+
+print(saldo > saque)
+
+print(saldo < saque)
+
+print(saldo >= saque)
+
+print(saldo <= saque)
+
+x = 2 * 4
+y = 24/3
+
+print(x == y)
+
+z = 2 * 4 == 24/3
+print(z)
+
+A = 15 % 4 < 19 % 6
+print(A)
+
+B = 25 * 5 >= 100 + 20
+print(B)
+
+C = 25 * 5 == 5 ** 3
+print(C)
