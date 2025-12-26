53269b74222f3e4bbd5199fb0071cfd5c7271dc8
Rebeca Sena <r3b3casena@gmail.com>
2025-12-26 10:48:37 -0300

    feat: adicionando arquivo operadores_Associacao.py

---
+++ operadores_Associacao.py
@@ -0,0 +1,13 @@
+frutas = ["limão", "Banana", "uva"]
+
+print("laranja" in frutas)
+#Ele é sensitive, ou seja, diferencia maiúsculas de minúsculas
+print("Banana" in frutas)
+print("limão" not in frutas)
+
+
+bilhete = [1, 2, 3, 4, 5]
+
+print(3 in bilhete)
+print(10 not in bilhete)
+print(0 in bilhete)
