403c148419efc964e60c73a06a344435e12e8c82
Rebeca Sena <r3b3casena@gmail.com>
2026-01-08 23:25:46 -0300

    Feat: Adicionando arquivo String_4.py

---
+++ String_4.py
@@ -0,0 +1,20 @@
+nome = "Ochaco Uraraka"
+
+mensagem = f"""
+Olá, meu nome é {nome},
+Eu estou aprendendo Python,
+      Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+print("""
+      ========================== Menu ==========================
+      |   Opção 1 - Depositar
+      |   Opção 2 - Sacar
+      |   Opção 3 - Extrato
+      |   Opção 4 - Sair
+      =========================================================
+
+      
+      """)
