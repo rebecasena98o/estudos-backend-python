11e4f9fd9a4bcfcd64fe6276a18b2e00cae8fd73
Rebeca Sena <r3b3casena@gmail.com>
2026-01-08 22:51:44 -0300

    Feat: adicionando arquivo String_3.py

---
+++ String_3.py
@@ -0,0 +1,15 @@
+nome = "Izuku Midoryia de lima "
+
+print(nome[0]) #I
+
+print(nome[:5]) #Izuku
+
+print(nome[13:]) #a de lima
+
+print(nome[10:16]) #ryia d
+
+print(nome[10:16:2]) #ri
+
+print(nome[:]) #Izuku Midoryia de lima
+
+print(nome[::-1]) # amil ed airyoiM ukuzI
