d46f8d01396042cecea8b8ffd68ecb4bbb75bd09
Rebeca Sena <r3b3casena@gmail.com>
2026-01-09 19:26:30 -0300

    Feat: adicionando arquivo extend_Listas.py

---
+++ extend_Listas.py
@@ -0,0 +1,9 @@
+### [].extend
+
+linguagens = ["python", "js", "Java"]
+
+print(linguagens) # ["python", "js", "Java"]
+
+linguagens.extend(["HTML", "CSS"])
+
+print(linguagens) # ["python", "js", "Java", "HTML", "CSS"]
